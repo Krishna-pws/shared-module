04492a87720aecc868f55f8b7c36f2b79e11f788
Krishna V <krishna.v@PACEWISDOM.LAB>
2025-04-17 17:14:33 +0530

    add profile interface

--- interfaces/user.interface.ts
+++ interfaces/user.interface.ts
@@ -1,21 +1,23 @@
 export interface User {
-    name: string;
-    email: string;
-    password: string;
-  }
-  
-  export interface AuthState {
-    isAuthenticated: boolean;
-    user: Omit<User, "password"> | null;
-    token: string | null;
-    loading: boolean;
-    error: string | null;
-    message: string | null;
-  }
- export interface SignupThunkInterface {
-    message: string;
-  }
-  
-export  interface LoginThunkInterface {
-    token: string;
-  }
+  id?: string;
+  name: string;
+  email: string;
+  password: string;
+  image?: string;
+}
+
+export interface AuthState {
+  isAuthenticated: boolean;
+  user: Omit<User, "password"> | null;
+  token: string | null;
+  loading: boolean;
+  error: string | null;
+  message: string | null;
+}
+export interface SignupThunkInterface {
+  message: string;
+}
+
+export interface LoginThunkInterface {
+  token: string;
+}
